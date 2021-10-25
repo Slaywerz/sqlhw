83613ef292a6cf22a0a20b8e3a15e23fcf0c38ce
Rustem Mirzakaev <rustem.mirzakaev@gmail.com>
2021-10-26 01:59:01 +0300

    DashboardPage added

---
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -0,0 +1,13 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+
+public class DashboardPage {
+    private SelenideElement heading = Selenide.$("[data-test-id='dashboard']");
+
+    public DashboardPage(){
+        heading.shouldBe(Condition.visible);
+    }
+}
