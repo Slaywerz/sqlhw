2962c4071131fb032d706b37260a2dae04e46594
Rustem Mirzakaev <rustem.mirzakaev@gmail.com>
2021-10-26 01:58:46 +0300

    VerificationPage added

---
+++ src/test/java/ru/netology/page/VerificationPage.java
@@ -0,0 +1,29 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.data.DataHelper;
+
+public class VerificationPage {
+    private SelenideElement codeField = Selenide.$("[data-test-id='code'] input");
+    private SelenideElement verifyButton = Selenide.$("[data-test-id='action-verify']");
+    private SelenideElement errorMessage = Selenide.$("[data-test-id='error-notification']");
+
+    public VerificationPage(){
+        codeField.shouldBe(Condition.visible);
+    }
+
+    public DashboardPage validVerify(){
+        codeField.setValue(null);
+        verifyButton.click();
+        return new DashboardPage();
+    }
+
+    public void invalidVerify(){
+        codeField.setValue(null);
+        verifyButton.click();
+        errorMessage.shouldBe(Condition.visible);
+    }
+
+}
