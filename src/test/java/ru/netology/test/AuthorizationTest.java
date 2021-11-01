bf2120a6f5de57b632fa11aae6b58c10ea68089d
Rustem Mirzakaev <rustem.mirzakaev@gmail.com>
2021-11-01 23:17:55 +0300

    methods to tests added

--- src/test/java/ru/netology/test/AuthorizationTest.java
+++ src/test/java/ru/netology/test/AuthorizationTest.java
@@ -1,9 +1,7 @@
 package ru.netology.test;
 
 import com.codeborne.selenide.Selenide;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import ru.netology.data.DataHelper;
 import ru.netology.page.LoginPage;
 
@@ -21,11 +19,33 @@ public class AuthorizationTest {
     }
 
     @Test
-    void wew() {
-    var loginPage = new LoginPage();
-    var authInfo = DataHelper.getAuthInfo();
-    var verificationPage = loginPage.validLogin(authInfo);
-    var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-    var dashboardPage = verificationPage.validVerify(verificationCode);
+    @DisplayName("Success authorization")
+    void shouldBeSuccessAuth() {
+        var loginPage = new LoginPage();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        var dashboardPage = verificationPage.validVerify(verificationCode);
+    }
+
+
+    @Test
+    @DisplayName("Visible error notification and blocking user after input wrong password 3 times")
+    void shouldBlockedUser() {
+        var loginPage = new LoginPage();
+        var authInfo = DataHelper.getInvalidAuthInfo();
+        var verificationPage = loginPage.invalidLoginInfo(authInfo);
+        var status = DataHelper.getStatus(authInfo);
+        Assertions.assertEquals("blocked", status);
+    }
+
+    @Test
+    @DisplayName("Invalid verification after input wrong verification code")
+    void shouldBeVerificationError() {
+        var loginPage = new LoginPage();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.invalidVerificationCode();
+        var dashboardPage = verificationPage.invalidVerify(verificationCode);
     }
 }
